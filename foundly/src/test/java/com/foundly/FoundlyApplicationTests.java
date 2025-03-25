7ae9f20ad1731780057c0116d9ba5a533cbdb2cb
Subham <120892516+subhamsubedi1943@users.noreply.github.com>
2025-03-25 12:33:15 +0530

    Add files via upload

---
+++ foundly/src/test/java/com/foundly/FoundlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
