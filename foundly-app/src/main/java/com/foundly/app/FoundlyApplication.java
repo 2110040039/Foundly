410151e7ef1a459b672d330f3fbb49d6f48fb74c
A Roopa Naga Lakshmi <110653243+2110040039@users.noreply.github.com>
2025-03-26 14:38:03 +0530

    Add files via upload

---
+++ foundly-app/src/main/java/com/foundly/app/FoundlyApplication.java
@@ -0,0 +1,13 @@
+package com.foundly.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoundlyApplication.class, args);
+        boolean f=true;
+        int t=10;
+    }
+}
