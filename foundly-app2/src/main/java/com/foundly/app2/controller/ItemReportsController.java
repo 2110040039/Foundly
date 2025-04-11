d31059d2fd0a907c08e7cddb74d08357f8eb5054
Akanksha <akankshakodipaka234@gmail.com>
2025-04-11 17:12:14 +0530

    backend

--- foundly-app2/src/main/java/com/foundly/app2/controller/ItemReportsController.java
+++ foundly-app2/src/main/java/com/foundly/app2/controller/ItemReportsController.java
@@ -1,19 +1,26 @@
 package com.foundly.app2.controller;
 
-import com.foundly.app2.entity.ItemReports;
-import com.foundly.app2.entity.Category;
-import com.foundly.app2.entity.User;
-import com.foundly.app2.service.ItemReportsService;
-import com.foundly.app2.service.CategoryService;
-import com.foundly.app2.service.UserService;
-import com.foundly.app2.dto.FoundItemReportRequest;
-import com.foundly.app2.dto.LostItemReportRequest;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.foundly.app2.dto.FoundItemReportRequest;
+import com.foundly.app2.dto.LostItemReportRequest;
+import com.foundly.app2.entity.Category;
+import com.foundly.app2.entity.ItemReports;
+import com.foundly.app2.entity.User;
+import com.foundly.app2.service.CategoryService;
+import com.foundly.app2.service.ItemReportsService;
+import com.foundly.app2.service.UserService;
 
 @RestController
 @RequestMapping("/api/items")
